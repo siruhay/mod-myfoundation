02493b2bfd8e2c25c9bc3c193cd3ae2f4639921b
sudarmono <30992530+monoland@users.noreply.github.com>
2025-10-08 22:26:34 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,11 @@ use Module\MyFoundation\Http\Controllers\MyFoundationOfficialController;
 use Module\MyFoundation\Http\Controllers\MyFoundationCommunityController;
 
 Route::get('dashboard', [DashboardController::class, 'index']);
+Route::post('upload-document', [DashboardController::class, 'upload']);
+Route::get('upload-document', [DashboardController::class, 'download']);
+Route::delete('upload-document', [DashboardController::class, 'destroy']);
 Route::get('report', [DashboardController::class, 'report']);
+
 Route::post('community/{myFoundationCommunity}/upload', [MyFoundationCommunityController::class, 'upload']);
 Route::resource('community', MyFoundationCommunityController::class)->parameters(['community' => 'myFoundationCommunity']);
 Route::resource('community.member', MyFoundationMemberController::class)->parameters(['community' => 'myFoundationCommunity', 'member' => 'myFoundationMember']);
--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace Module\MyFoundation\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardController extends Controller
 {
@@ -19,6 +20,85 @@ class DashboardController extends Controller
     }
 
     /**
+     * upload function
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function upload(Request $request)
+    {
+        $request->validate([
+            'file' => "required|file|max:2048"
+        ]);
+
+        if ($request->hasFile('file') && $request->file('file')) {
+            $fileslug = $request->slug;
+            $filename = $request->uuid . $request->extension;
+            $filepath = $fileslug . DIRECTORY_SEPARATOR . $filename;
+
+            if (Storage::disk('uploads')->putFileAs($fileslug, $request->file('file'), $filename)) {
+                return response()->json([
+                    'path' => $filepath
+                ], 200);
+            }
+        }
+
+        return response()->json([
+            'status' => 422,
+            'message' => 'Upload file bermasalah'
+        ], 422);
+    }
+
+    /**
+     * download function
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function download(Request $request)
+    {
+        if (!Storage::disk('uploads')->exists($request->path)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'File not found'
+            ], 404);
+        }
+
+        return optional(Storage::disk('uploads'))->download($request->path, 'sk-ppk.pdf', [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="sample.pdf"',
+        ]);
+    }
+
+    /**
+     * destroy function
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function destroy(Request $request)
+    {
+        if (!Storage::disk('uploads')->exists($request->path)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'File not found'
+            ], 404);
+        }
+
+        if (Storage::disk('uploads')->delete($request->path)) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Hapus file dari server berhasil.'
+            ], 200);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Hapus file dari server gagal.'
+        ], 500);
+    }
+
+    /**
      * Undocumented function
      *
      * @param Request $request
@@ -26,6 +106,6 @@ class DashboardController extends Controller
      */
     public function report(Request $request): void
     {
-        // 
+        //
     }
 }
